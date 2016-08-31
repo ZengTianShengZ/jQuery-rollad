e124a2087c843e7f99dad5e6557b7d05b1ca7e2d
ZengTianShengZ <zengtianshengz@163.com>
2016-08-31 19:43:53 +0800

    定时轮播，个性化定制

--- jQuery-rollad/js/jquery.roll-ad.js
+++ jQuery-rollad/js/jquery.roll-ad.js
@@ -12,6 +12,18 @@
         this.prevBtn =  rollAd.find('.roll-prev-btn');
         this.nextBtn =  rollAd.find('.roll-next-btn');
 
+       this.rollItemLen =  this.rollItems.length;
+        //默认参数
+        this.setting = {
+            "width":700,
+            "height":150,
+            "liWidth":320,
+            "liHeight":120,
+            "delay":1000
+        }
+        //自定义参数与默认参数合并，jq 的 extend 方法
+        $.extend(this.setting,this.getSetting())
+
         this.initPosition();
 
         this.rollFlag = true;
@@ -29,18 +41,39 @@
             }
         });
 
+        this.autoRoll();
+        this.rollAd.hover(function(){
+            clearInterval(self.timer);
+        },function(){
+            self.autoRoll();
+        });
 
     };
     RollAd.prototype = {
 
+        autoRoll:function () {
+            var roll_this =  this;
+            this.timer = window.setInterval(function () {
+                  roll_this.nextBtn.click();
+              },roll_this.setting.delay);
+        },
         initPosition:function () {
+            var rollWidth = this.setting.width;
+            var rollHeight = this.setting.height;
+            var liWidth = this.setting.liWidth;
+            var liHeight = this.setting.liHeight;
+            var firPos =  (rollWidth - liWidth - 20 )/2;
 
-            var firPos =  (800 - 520 - 20 )/2;
+            this.rollAd.css({"width":rollWidth,"height":rollHeight});
+
+            this.rollItems.each(function (index,item) {
+                $(this).css({"width":liWidth,"height":liHeight,"left":firPos});
+            });
 
             this.firstRollItem.css({
                 "left":firPos,
-                "width":520+20,
-                "height":220+10,
+                "width":liWidth+20,
+                "height":liHeight+10,
                 "opacity":1,
                 //Math.ceil 向上取整
                 "zIndex":33,
@@ -49,7 +82,7 @@
 
             console.log("..gg.nowflag... 000.");
             this.secondRollItem.css({
-                "left":800 - 520,
+                "left":rollWidth - liWidth,
                 "opacity":0.5,
                 "zIndex":22,
                 "top":10
@@ -67,13 +100,19 @@
 
         rotateAnimate:function (type) {
 
-            if(parseInt(this.nowflag) >= 7){
+            var rollWidth = this.setting.width;
+            var rollHeight = this.setting.height;
+            var liWidth = this.setting.liWidth;
+            var liHeight = this.setting.liHeight;
+            var delay =  this.setting.delay;
+
+            if(parseInt(this.nowflag) >= this.rollItemLen){
 
                 this.nowflag = 0;
 
             }else if(parseInt(this.nowflag) < 0){
 
-                this.nowflag = 6;
+                this.nowflag = this.rollItemLen - 1;
             }
             console.log(".... this.nowflag...."+  this.nowflag);
             var nowEle = this.rollItems.eq(this.nowflag).removeAttr('nowflag');
@@ -84,27 +123,27 @@
             if(type == 'next'){
                 var nextEleTwo =  nextEle.next().get(0)? nextEle.next() : this.firstRollItem;
 
-                nextEleTwo.animate(this.getEleCss(nextEle),1000,function () {
+                nextEleTwo.animate(this.getEleCss(nextEle),delay,function () {
                     that.rollFlag = true;
                 });
 
-                nextEle.animate(this.getEleCss(nowEle),1000,function () {
+                nextEle.animate(this.getEleCss(nowEle),delay,function () {
                     that.rollFlag = true;
                 }).attr('nowflag',++this.nowflag);
 
-                nowEle.css("opacity","22");
-                nowEle.animate(this.getEleCss(prev),1000,function () {
+               // nowEle.css("opacity","22");
+                nowEle.animate(this.getEleCss(prev),delay,function () {
                     that.rollFlag = true;
                 });
 
                 prev.animate({
-                    "width":"520px",
-                    "height":"220px",
+                    "width":liWidth,
+                    "height":liHeight,
                     "zIndex":0,
                     "left":"140px",
                     "opacity":0,
                     "top":"10px"
-                },1000,function () {
+                },delay,function () {
                     that.rollFlag = true;
                 });
 
@@ -114,27 +153,27 @@
                 var prevEleTwo =  prev.prev().get(0)? prev.prev() : this.lastRollItem;
 
 
-                prevEleTwo.animate(this.getEleCss(prev),1000,function () {
+                prevEleTwo.animate(this.getEleCss(prev),delay,function () {
                     that.rollFlag = true;
                 });
 
-                prev.animate(this.getEleCss(nowEle),1000,function () {
+                prev.animate(this.getEleCss(nowEle),delay,function () {
                     that.rollFlag = true;
                 }).attr('nowflag',--this.nowflag);
 
                // nowEle.css("opacity","22");
-                nowEle.animate(this.getEleCss(nextEle),1000,function () {
+                nowEle.animate(this.getEleCss(nextEle),delay,function () {
                     that.rollFlag = true;
                 });
 
                 nextEle.animate({
-                    "width":"520px",
-                    "height":"220px",
+                    "width":liWidth,
+                    "height":liHeight,
                     "zIndex":0,
                     "left":"140px",
                     "opacity":0,
                     "top":"10px"
-                },1000,function () {
+                },delay,function () {
                     that.rollFlag = true;
                 });
 
@@ -156,6 +195,16 @@
                 "opacity":opacity,
                 "top":top
             }
+        },
+        getSetting:function () {
+            var setting = this.rollAd.attr('ata-setting');
+            if(setting != undefined){
+                // 解析 json ，返回 obj  ， javascript 的 json 数据 跟对象一样一样的
+                return $.parseJSON(setting);
+            }else{
+                return {} ;
+            }
+
         }
 
 
